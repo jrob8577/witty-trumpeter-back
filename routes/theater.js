e1d9885781e032b3581fb616a3b20b8d1885fe60
Devon Wesley <dwesley89@gmail.com>
2016-08-01 16:00:04 -0700

    added showtimeapi package & calls to api

--- routes/theater.js
+++ routes/theater.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Showtimes = require('showtimes');
 
 /* GET theater calls. */
-router.get('/list/:zipcode', (req, res, next) => {
-  res.send({theater: 'list'})
+router.get('/list/:zipcode', (request, response, next) => {
+  const { zipcode } = request.params
+
+  const api = new Showtimes(zipcode, {});
+
+  api.getTheaters((error, theaters) => {
+    if (error) throw error
+    response.send(theaters);
+  });
 });
 
-router.get('/:id', (req, res, next) => {
-  res.send({theater: 'single-theater'})
+router.get('/:id', (request, response, next) => {
+  const { id } = request.params
+
+  const api = new Showtimes();
+
+  api.getTheater(id, (error, theaters) => {
+    if (error) throw error
+    response.send(theaters);
+  });
 });
 
 module.exports = router;
